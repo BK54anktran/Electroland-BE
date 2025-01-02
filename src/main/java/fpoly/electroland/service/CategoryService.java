c02edaa9130a2a50bc2546ac8ec7bd16b10a677a
Trần Ngọc Anh <ankankbk54@gmail.com>
2025-01-02 15:13:07 +0700

    Update_Category

--- src/main/java/fpoly/electroland/service/CategoryService.java
+++ src/main/java/fpoly/electroland/service/CategoryService.java
@@ -1,8 +1,11 @@
 package fpoly.electroland.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fpoly.electroland.model.Category;
 import fpoly.electroland.repository.CategoryReponsitory;
 
 @Service
@@ -11,4 +14,7 @@ public class CategoryService {
     @Autowired
     CategoryReponsitory categoryReponsitory;
 
+    public List<Category> getAll() {
+        return categoryReponsitory.findAll();
+    }
 }
