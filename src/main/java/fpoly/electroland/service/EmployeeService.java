43b3abcfa879a2062ed5fb7051f8b547be42005b
Trần Ngọc Anh <ankankbk54@gmail.com>
2025-01-08 13:58:08 +0700

    Update_Employee_service

--- src/main/java/fpoly/electroland/service/EmployeeService.java
+++ src/main/java/fpoly/electroland/service/EmployeeService.java
@@ -1,47 +1,29 @@
 package fpoly.electroland.service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-import fpoly.electroland.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import fpoly.electroland.model.Employee;
 import fpoly.electroland.repository.EmployeeReponsitory;
-import fpoly.electroland.util.JwtUtil;
-import fpoly.electroland.util.ResponseEntityUtil;
 
 @Service
 public class EmployeeService {
 
-    private final JwtUtil jwtUtil;
-    private final EmployeeReponsitory employeeReponsitory;
-    private final AuthenticationManager authenticationManager;
-
     @Autowired
-    public EmployeeService(JwtUtil jwtUtil, EmployeeReponsitory employeeReponsitory, @Lazy AuthenticationManager authenticationManager) {
-        this.jwtUtil = jwtUtil;
-        this.employeeReponsitory = employeeReponsitory;
-        this.authenticationManager = authenticationManager;
+    EmployeeReponsitory employeeReponsitory;
+
+    public List<Employee> getAll() {
+        return employeeReponsitory.findAll();
     }
 
-    public List<Employee> getAll(){
-        return employeeReponsitory.findAll();
-   }
-
-   
     public Optional<Employee> getEmployee(String email) {
         Optional<Employee> employee = employeeReponsitory.findByEmail(email);
         return employee;
     }
+
     // Thêm mới nhân viên
     public Employee createEmployee(Employee employee) {
         return employeeReponsitory.save(employee);
@@ -49,12 +31,12 @@ public class EmployeeService {
 
     public void deleteEmployee(Long id) {
         if (employeeReponsitory.existsById(id)) { // Kiểm tra xem ID có tồn tại không
-            employeeReponsitory.deleteById(id);  // Xóa nhân viên theo ID
+            employeeReponsitory.deleteById(id); // Xóa nhân viên theo ID
         } else {
             throw new RuntimeException("Nhân viên với ID " + id + " không tồn tại!");
         }
     }
-    
+
     // Sửa nhân viên
     public Employee updateEmployee(Long id, Employee updatedEmployee) {
         Optional<Employee> optionalEmployee = employeeReponsitory.findById(id);
@@ -68,27 +50,6 @@ public class EmployeeService {
             return employeeReponsitory.save(existingEmployee);
         } else {
             throw new RuntimeException("Employee not found with id: " + id);
-        }
-    }
-
-    public Object authentication_getData(String email, String password) {
-        try {
-            // Sử dụng biến cục bộ thay vì thuộc tính toàn cục
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(email, password));
-            User user = (User) authentication.getPrincipal();
-
-            Employee employee = new Employee();
-            employee.setEmail(user.getEmail());
-            employee.setFullName(user.getName());
-
-            Map<String, String> data = new HashMap<>();
-            data.put("token", jwtUtil.generateToken(employee.getEmail()));
-            data.put("userName", employee.getFullName());
-            return ResponseEntity.ok(data);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntityUtil.unauthorizedError("Mật khẩu không chính xác");
         }
     }
 }
