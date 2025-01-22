b889675c32af5de836dde7d6300d6291eccec26f
MinhNgoc <minhngoc9499@gmail.com>
2025-01-23 01:47:18 +0700

    update

--- src/main/java/fpoly/electroland/util/CreateAction.java
+++ src/main/java/fpoly/electroland/util/CreateAction.java
@@ -7,26 +7,27 @@ import fpoly.electroland.model.Action;
 import fpoly.electroland.model.Employee;
 import fpoly.electroland.repository.ActionRepository;
 import fpoly.electroland.service.ActionService;
+
 @Component
-public class CreateAction{
+public class CreateAction {
     @Autowired
     ActionService actionService;
+
     public void createAction(String tableName, String actionName, int objectID, String oldValue, String newValue,
-    Employee creatorEmployee) {
-// Tạo bản ghi Action
-Action action = new Action();
-action.setTableName(tableName);
-action.setAction(actionName);
-action.setIdRecord(objectID);
-action.setOldValue(oldValue); // Gắn giá trị cũ
-action.setNewValue(newValue); // Gắn giá trị mới
-action.setEmployee(creatorEmployee); // Gắn nhân viên tạo vào Action
+            Employee creatorEmployee) {
+        // Tạo bản ghi Action
+        Action action = new Action();
+        action.setTableName(tableName);
+        action.setAction(actionName);
+        action.setIdRecord(objectID);
+        action.setOldValue(oldValue); // Gắn giá trị cũ
+        action.setNewValue(newValue); // Gắn giá trị mới
+        action.setEmployee(creatorEmployee); // Gắn nhân viên tạo vào Action
 
-// Lưu Action vào cơ sở dữ liệu
-actionService.saveAction(action);
+        // Lưu Action vào cơ sở dữ liệu
+        actionService.saveAction(action);
 
-// Log thông tin
-System.out.println("Action created: " + action);
+        // Log thông tin
+        System.out.println("Action created: " + action);
+    }
 }
-}
-
