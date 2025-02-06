5cf701c3d057f8478dd3963534852b3e6ad12db4
SeaTurtle169 <donganh16997@gmail.com>
2025-02-06 14:14:52 +0700

    commit 06/2

---
+++ src/main/java/fpoly/electroland/restController/ReviewController.java
@@ -0,0 +1,20 @@
+package fpoly.electroland.restController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import fpoly.electroland.service.ReviewService;
+import fpoly.electroland.util.ResponseEntityUtil;
+
+@RestController
+public class ReviewController {
+    @Autowired
+    ReviewService reviewService;
+
+    @GetMapping("/reviews")
+    public Object getMethodName(@RequestParam(name = "id", required = false, defaultValue = "0") int id) {
+        return ResponseEntityUtil.ok(reviewService.getReviewsByproductId(id));
+    }
+}
