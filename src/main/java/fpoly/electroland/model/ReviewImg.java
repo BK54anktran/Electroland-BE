43adc58a38a79763d684c240e434fed7b3d87656
Trần Ngọc Anh <ankankbk54@gmail.com>
2025-02-09 19:25:42 +0700

    Squashed commit of the following:
    commit 5ccd55bc0b8284fb219142bd2e225ade9508cdc8
    Author: SeaTurtle169 <donganh16997@gmail.com>
    Date:   Sun Feb 9 18:06:31 2025 +0700
    
        commit done
    
    commit df3bb9f2a40e88db820d481908c7fbd257ad7fa6
    Author: SeaTurtle169 <donganh16997@gmail.com>
    Date:   Sat Feb 8 13:19:11 2025 +0700
    
        commit 08/2
    
    commit 5cf701c3d057f8478dd3963534852b3e6ad12db4
    Author: SeaTurtle169 <donganh16997@gmail.com>
    Date:   Thu Feb 6 14:14:52 2025 +0700
    
        commit 06/2

---
+++ src/main/java/fpoly/electroland/model/ReviewImg.java
@@ -0,0 +1,34 @@
+package fpoly.electroland.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ReviewImg {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+    @Column(columnDefinition = "NVARCHAR(225)")
+	private String nameIMG;
+    
+    @ManyToOne
+	@JoinColumn(name = "idReview")
+    @JsonIgnore
+	private Review review;
+}
--- src/main/java/fpoly/electroland/service/ProductService.java
+++ src/main/java/fpoly/electroland/service/ProductService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fpoly.electroland.model.Category;
-import fpoly.electroland.model.Employee;
 import fpoly.electroland.model.Product;
 import fpoly.electroland.model.Supplier;
 import fpoly.electroland.repository.CategoryRepository;
