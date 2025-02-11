2124f4ca94c0be46eb1af7168951549eb2835ce6
SeaTurtle169 <donganh16997@gmail.com>
2025-02-11 20:58:59 +0700

    commit 11/2

--- src/main/java/fpoly/electroland/restController/CustomerController.java
+++ src/main/java/fpoly/electroland/restController/CustomerController.java
@@ -4,21 +4,46 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fpoly.electroland.model.Customer;
+import fpoly.electroland.model.Product;
 import fpoly.electroland.service.CustomerService;
+import fpoly.electroland.service.UserService;
+import fpoly.electroland.util.ResponseEntityUtil;
 
 @RestController
-@RequestMapping("/admin")
 public class CustomerController {
     @Autowired
     CustomerService customerService;
 
-    @GetMapping("/customer")
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/admin/customer")
     public List<Customer> GetAllList() {
         return customerService.getAll();
     }
 
+    @GetMapping("/userinfor")
+    public Customer getUser() {
+        System.out.println(userService.getUser());
+        Customer customer = customerService.getCustomer(userService.getUser().getId()).get();
+        return customer;
+    }
+
+    @PostMapping("/userAvatar")
+    public void saveUserAva(@RequestBody String avaUrl) {
+        avaUrl = avaUrl.replace("\"", "");
+        System.out.println(userService.getUser());
+        int id = userService.getUser().getId();
+        Customer customer = customerService.getCustomer(userService.getUser().getId()).get();
+        customer.setAvatar(avaUrl);
+        customerService.updateCustomer(id, customer);
+        System.out.println(avaUrl);
+    }
 }
