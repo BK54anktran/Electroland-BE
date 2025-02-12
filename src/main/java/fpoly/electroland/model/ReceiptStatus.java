93cc16c1a6e66aa144116971bf01cb8346381f9f
Trần Ngọc Anh <ankankbk54@gmail.com>
2025-02-12 23:55:40 +0700

    Squashed commit of the following:
    commit d8d876db4627010f81cfbb386b22994fd7ded6b8
    Author: MinhNgoc <minhngoc9499@gmail.com>
    Date:   Wed Feb 12 02:09:50 2025 +0700
    
        update password
    
    commit 8995b129b82aa284edda675e61dacb53512e3ccc
    Author: MinhNgoc <minhngoc9499@gmail.com>
    Date:   Mon Feb 10 06:33:10 2025 +0700
    
        update receipt
    
    commit 9d83454841756df4ef0cbba6096ec1e702c6ff2a
    Author: MinhNgoc <minhngoc9499@gmail.com>
    Date:   Mon Feb 10 01:07:15 2025 +0700
    
        update apt
    
    commit 4af6713c80712a091068719953ae7c3bfec55a1b
    Merge: de8c235 1f55fa2
    Author: MinhNgoc <minhngoc9499@gmail.com>
    Date:   Sat Feb 8 23:31:47 2025 +0700
    
        Merge branch 'Developer' into BE_Ngoc_OrderManagement
    
    commit de8c23541722cd783b6250cbd012309cf9e5b892
    Author: MinhNgoc <minhngoc9499@gmail.com>
    Date:   Sat Feb 8 03:37:59 2025 +0700
    
        Update receidetail
    
    commit f08c4a05c361012742f2c2f721723608718ee0f6
    Author: MinhNgoc <minhngoc9499@gmail.com>
    Date:   Thu Feb 6 19:05:13 2025 +0700
    
        update
    
    commit 16ceb9fa835d7c339270d408cd7b916dc9eb0d13
    Author: MinhNgoc <minhngoc9499@gmail.com>
    Date:   Wed Feb 5 21:52:38 2025 +0700
    
        update

--- src/main/java/fpoly/electroland/model/ReceiptStatus.java
+++ src/main/java/fpoly/electroland/model/ReceiptStatus.java
@@ -1,12 +1,25 @@
 package fpoly.electroland.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ReceiptStatus {
 
     @Id
@@ -15,4 +28,8 @@ public class ReceiptStatus {
 
     @Column(nullable = false, columnDefinition = "nvarchar(225)")
     String name;
+
+    @OneToMany(mappedBy = "receiptStatus")
+    @JsonIgnore
+    private List<Receipt> receipts;
 }
--- src/main/java/fpoly/electroland/service/ActionService.java
+++ src/main/java/fpoly/electroland/service/ActionService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fpoly.electroland.model.Action;
+import fpoly.electroland.model.Employee;
 import fpoly.electroland.repository.ActionRepository;
 
 @Service
@@ -17,5 +18,11 @@ public class ActionService {
 
     public void saveAction(Action action) {
         actionRepository.save(action);
+    }
+
+    public void createAction(String string, String string2, int id, String oldValue, String string3,
+            Employee creatorEmployee) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createAction'");
     }
 }
