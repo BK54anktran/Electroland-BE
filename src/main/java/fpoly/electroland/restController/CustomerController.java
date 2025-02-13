507ac39eec43f89f799e310dd4332f86c6840797
Trần Ngọc Anh <ankankbk54@gmail.com>
2025-02-13 17:25:23 +0700

    Squashed commit of the following:
    commit 1933240a91bcff208b9c9cd8a08bf512b8215f38
    Author: SeaTurtle169 <donganh16997@gmail.com>
    Date:   Thu Feb 13 16:46:38 2025 +0700
    
        commit 13/2
    
    commit 2124f4ca94c0be46eb1af7168951549eb2835ce6
    Author: SeaTurtle169 <donganh16997@gmail.com>
    Date:   Tue Feb 11 20:58:59 2025 +0700
    
        commit 11/2

--- src/main/java/fpoly/electroland/restController/CustomerController.java
+++ src/main/java/fpoly/electroland/restController/CustomerController.java
@@ -1,42 +1,46 @@
 package fpoly.electroland.restController;
 
+import java.sql.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fpoly.electroland.model.Customer;
+import fpoly.electroland.model.Product;
 import fpoly.electroland.service.CustomerService;
+import fpoly.electroland.service.UserService;
+import fpoly.electroland.util.DateUtil;
 
 @RestController
-@RequestMapping("/admin")
 public class CustomerController {
     @Autowired
     CustomerService customerService;
 
-    @GetMapping("/customer")
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/admin/customer")
     public List<Customer> GetAllList() {
         return customerService.getAll();
     }
-      @PostMapping("/customer/save")
+
+    @PostMapping("/customer/save")
     public Customer addCustomer(@RequestBody Customer customer) {
         return customerService.createCustomer(customer);
     }
 
     @PutMapping("/customer/update/{id}")
-    public ResponseEntity<Customer> updateCustomer(@PathVariable Integer id,@RequestParam boolean status) {
-     
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Integer id, @RequestParam boolean status) {
+
         Customer updatedCustomer = customerService.updateCustomer(id, status);
-        
+
         if (updatedCustomer != null) {
             return ResponseEntity.ok(updatedCustomer);
         } else {
@@ -63,4 +67,43 @@ public class CustomerController {
         return customerService.searchAndFilterCustomers(keyword, status);
     }
 
+    @GetMapping("/userinfor")
+    public Customer getUser() {
+        System.out.println(userService.getUser());
+        Customer customer = customerService.getCustomer(userService.getUser().getId()).get();
+        return customer;
+    }
+
+    @PostMapping("/userAvatar")
+    public void saveUserAva(@RequestBody String avaUrl) {
+        avaUrl = avaUrl.replace("\"", "");
+        System.out.println(userService.getUser());
+        int id = userService.getUser().getId();
+        Customer customer = customerService.getCustomer(userService.getUser().getId()).get();
+        customer.setAvatar(avaUrl);
+        customerService.updateCustomer(id, customer);
+        System.out.println(avaUrl);
+    }
+
+    @PostMapping("/userUpdate")
+    public void updateUser(@RequestBody Map<String, Object> object) {
+        String dateOfBirthStr = (String) object.get("dateOfBirth"); // Lấy ngày dưới dạng chuỗi
+        Date dateOfBirth = (Date) DateUtil.formatDate(dateOfBirthStr);
+        String fullName = (String) object.get("fullName");
+        Boolean gender = (Boolean) object.get("gender");
+        String phoneNumber = (String) object.get("phoneNumber");
+
+        int id = userService.getUser().getId();
+        Customer customer = customerService.getCustomer(userService.getUser().getId()).get();
+        customer.setDateOfBirth(dateOfBirth);
+        customer.setGender(gender);
+        customer.setPhoneNumber(phoneNumber);
+        customer.setFullName(fullName);
+        if (object.get("newPassword") != null) {
+            String newPassword = (String) object.get("newPassword");
+            customer.setPassword(newPassword);
+        }
+        System.out.println(customer);
+        customerService.updateCustomer(id, customer);
+    }
 }
