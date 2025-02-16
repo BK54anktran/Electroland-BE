83942d1d200ad36c7242813077fde0ba75d0e1d6
Trần Ngọc Anh <ankankbk54@gmail.com>
2025-02-16 18:42:04 +0700

    Update_Coupon

--- src/main/java/fpoly/electroland/service/CustomerCouponService.java
+++ src/main/java/fpoly/electroland/service/CustomerCouponService.java
@@ -1,8 +1,19 @@
 package fpoly.electroland.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import fpoly.electroland.dto.response.ProductCouponDto;
+import fpoly.electroland.dto.response.ReceiptCouponDto;
+import fpoly.electroland.model.CustomerCoupon;
+import fpoly.electroland.model.ProductCoupon;
+import fpoly.electroland.model.ReceiptCoupon;
 import fpoly.electroland.repository.CustomerCouponRepository;
 
 @Service
@@ -10,5 +21,35 @@ public class CustomerCouponService {
 
     @Autowired
     CustomerCouponRepository customerCouponRepository;
+
+    @Autowired
+    UserService userService;
+
+    public Object getList() {
+        List<CustomerCoupon> list = customerCouponRepository.findByCustomerId(userService.getUser().getId());
+        List<ProductCouponDto> listProductCoupon = new ArrayList<>();
+        List<ReceiptCouponDto> listReceiptCouponDto = new ArrayList<>();
+        for (CustomerCoupon customerCoupon : list) {
+            if (customerCoupon.getProductCoupon() != null) {
+                listProductCoupon.add(ProductCouponToDto(customerCoupon.getId(), customerCoupon.getProductCoupon()));
+            } else
+                listReceiptCouponDto.add(ReceiptCouponToDto(customerCoupon.getId(), customerCoupon.getReceiptCoupon()));
+        }
+        Map<String, Object> couponUser = new HashMap<>();
+        couponUser.put("productCoupon", listProductCoupon);
+        couponUser.put("receiptCoupon", listReceiptCouponDto);
+        ;
+        return couponUser;
+    }
+
+    ProductCouponDto ProductCouponToDto(int id, ProductCoupon productCoupon) {
+        return new ProductCouponDto(id, productCoupon.getProduct().getId(), productCoupon.getProduct().getAvatar(),
+                productCoupon.getDescription(), productCoupon.getValue());
+    }
+
+    ReceiptCouponDto ReceiptCouponToDto(int id, ReceiptCoupon receiptCoupon) {
+        return new ReceiptCouponDto(id, receiptCoupon.getDiscountMoney(), receiptCoupon.getDiscountPercent(),
+                receiptCoupon.getMaxDiscount(), receiptCoupon.getMinReceiptPrice(), receiptCoupon.getDescription());
+    }
 
 }
