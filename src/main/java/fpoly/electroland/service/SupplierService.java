f320cf5e43773130d5a21f1408d0991e755db9d8
TANi <Tani100924@gmail.com>
2025-03-28 22:45:17 +0700

    kiet

--- src/main/java/fpoly/electroland/service/SupplierService.java
+++ src/main/java/fpoly/electroland/service/SupplierService.java
@@ -1,12 +1,17 @@
 package fpoly.electroland.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fpoly.electroland.model.Employee;
+import fpoly.electroland.model.ProductCoupon;
 import fpoly.electroland.model.Supplier;
+import fpoly.electroland.repository.EmployeeRepository;
 import fpoly.electroland.repository.SupplierRepository;
+import fpoly.electroland.util.CreateAction;
 
 @Service
 public class SupplierService {
@@ -14,8 +19,42 @@ public class SupplierService {
     @Autowired
     SupplierRepository supplierRepository;
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Autowired
+    CreateAction createAction;
+
     public List<Supplier> getAllSuppliers() {
         return supplierRepository.findAll();
     }
 
+    public Supplier createSupplier(Supplier supplier, int userId){
+        Supplier savedSupplier = supplierRepository.save(supplier);
+        Employee creator = employeeRepository.findById(userId)
+            .orElseThrow(() -> new RuntimeException("User not found"));
+        createAction.createAction("Supplier", "CREATE", savedSupplier.getId(), null, 
+        savedSupplier.toString(), creator);
+        return supplierRepository.save(supplier);
+    }
+
+    public Supplier updateSupplier(Long id, Supplier updateSupplier, int userId) {
+        Optional<Supplier> optionalSupplier = supplierRepository.findById(id);
+        if (optionalSupplier.isPresent()) {
+            Supplier existingSupplier = optionalSupplier.get();
+            
+            existingSupplier.setLogo(updateSupplier.getLogo());
+            existingSupplier.setName(updateSupplier.getName());
+
+            Supplier savedSupplier = supplierRepository.save(existingSupplier);
+
+            Employee creatorEmployee = employeeRepository.findById(userId)
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+
+            createAction.createAction("Supplier", "UPDATE", savedSupplier.getId(), optionalSupplier.toString(), updateSupplier.toString(), creatorEmployee);
+            return savedSupplier;
+        } else {
+            throw new RuntimeException("Employee not found with id: " + id);
+        }
+    }
 }
