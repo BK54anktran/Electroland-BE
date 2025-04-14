88c7937b1617e0df0521356ca95d735a320846bf
MinhNgoc <minhngoc9499@gmail.com>
2025-04-15 02:28:06 +0700

    update receipt

--- src/main/java/fpoly/electroland/repository/ReceiptDetailRepository.java
+++ src/main/java/fpoly/electroland/repository/ReceiptDetailRepository.java
@@ -23,7 +23,7 @@ public interface ReceiptDetailRepository extends JpaRepository<ReceiptDetail, In
             "FROM ReceiptDetail rd " +
             "JOIN rd.receipt r " +
             "JOIN rd.product p " +
-            "WHERE r.receiptDate BETWEEN :startDate AND :endDate " +
+            "WHERE r.receiptStatus.id = 5 AND r.receiptDate BETWEEN :startDate AND :endDate " +
             "GROUP BY p.name " +
             "ORDER BY SUM(rd.quantity) DESC " +
             "LIMIT 10")
@@ -34,7 +34,7 @@ public interface ReceiptDetailRepository extends JpaRepository<ReceiptDetail, In
     // Truy vấn doanh thu từ bảng Payment, từ ngày đầu tháng đến ngày truyền vào
     @Query("SELECT SUM(p.amount) FROM Receipt r " +
             "JOIN r.payment p " +
-            "WHERE r.receiptStatus.id = 3 AND r.receiptDate >= :startDate AND r.receiptDate <= :endDate")
+            "WHERE r.receiptStatus.id = 5 AND r.receiptDate >= :startDate AND r.receiptDate <= :endDate")
     Long getRevenueByDateRange(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 
     // Truy vấn số lượng đơn hàng từ bảng Receipt, từ ngày đầu tháng đến ngày truyền
@@ -45,7 +45,7 @@ public interface ReceiptDetailRepository extends JpaRepository<ReceiptDetail, In
 
     // Truy vấn số lượng đơn thành công từ đầu tháng đến ngày truyền vào
     @Query("SELECT COUNT(r) FROM Receipt r " +
-            "WHERE r.receiptStatus.id = 3 AND r.receiptDate >= :startDate AND r.receiptDate <= :endDate")
+            "WHERE r.receiptStatus.id = 5 AND r.receiptDate >= :startDate AND r.receiptDate <= :endDate")
     Long getSuccessfulOrders(@Param("startDate") LocalDateTime startOfMonth, @Param("endDate") LocalDateTime endDate);
 
     // Truy vấn số lượng đơn hàng thất bại từ đầu tháng đến ngày truyền vào
@@ -64,7 +64,7 @@ public interface ReceiptDetailRepository extends JpaRepository<ReceiptDetail, In
                "FROM receipt r " +
                "JOIN receiptStatus rs ON r.idReceiptStatus = rs.id " +
                "JOIN payment p ON r.idPayment = p.id " +
-               "WHERE rs.id = 3 AND YEAR(r.receiptDate) = :year " +
+               "WHERE rs.id = 5 AND YEAR(r.receiptDate) = :year " +
                "GROUP BY MONTH(r.receiptDate) " +
                "ORDER BY month", nativeQuery = true)
 List<Object[]> getRevenueByMonth(@Param("year") int year);
